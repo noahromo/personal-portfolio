e24cea32478e864a775d26f5f10182ec51bade20
Noah Romo <noahromo17@gmail.com>
2022-06-26 01:36:50 -0400

    Working on single page portfolio and editing home design

--- app/__init__.py
+++ app/__init__.py
@@ -47,7 +47,7 @@ team = {'juan': {"firstname": "Juan's",
 
 @app.route('/')
 def index():
-    return render_template('home.html', title="MLH Fellow", url=os.getenv("URL"))
+    return render_template('home.html', title="Noah Romo", url=os.getenv("URL"))
 
 
 @app.route('/member/<member>')
