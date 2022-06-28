f0ff6307b1299901d1bf1ad346452fc8e5bed85d
Noah Romo <noahromo17@gmail.com>
2022-06-28 01:25:22 -0400

    Added MySQL database connection, and POST, GET, DELETE endpoints

---
+++ curl-test.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+curl -X Post http://localhost:5000/api/timeline_post -d 'name=Test&email=test@test.com&content=Testing my endpoints.'
+curl http://localhost:5000/api/timeline_post 
+curl -X DELETE http://localhost:5000/api/timeline_post
+curl http://localhost:5000/api/timeline_post 
