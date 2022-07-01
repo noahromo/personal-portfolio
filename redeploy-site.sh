125a2d1378e8e7d162986657e4002e155df771c7
Noah Romo <noahromo17@gmail.com>
2022-07-01 10:44:32 -0400

    Added new service to redeploy-site.sh in order to run the application on boot

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-tmux kill-server
 cd personal-portfolio
 git fetch && git reset origin/main --hard
 virtualenv env
 source env/bin/activate
 pip install -r requirements.txt
-tmux new -d 'source env/bin/activate && flask run --host=0.0.0.0'
+systemctl restart myportfolio
 cd
-clear 
+clear
